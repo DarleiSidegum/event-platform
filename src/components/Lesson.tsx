72443e44944bd1041d0fa31b3de2eadfd47edced
Darlei Sidegum <darleidasilvasidegum@hotmail.com>
2022-06-27 17:55:20 -0300

    ajustes

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,8 +1,8 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
-import { Link } from "react-router-dom";
-
+import { Link, useParams } from "react-router-dom";
+import classNames from "classnames";
 interface LessonProps {
   title: string;
   slug: string;
@@ -11,22 +11,36 @@ interface LessonProps {
 }
 
 export function Lesson({ title, slug, availableAt, type }: LessonProps) {
+  const { var_slug } = useParams<{ var_slug: string }>();
   const isLessonAvailable = isPast(availableAt);
   const availableDateFormatted = format(
     availableAt,
     "EEEE' • 'd' de 'MMMM' • 'k'h'mm",
     { locale: ptBR }
   );
+
+  const isActiveLesson = var_slug === slug;
+
   return (
     <Link to={`/event/lesson/${slug}`} className="group">
-      <span className="text-gray-300">
-        {availableAt.toString()}
-      </span>
-
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      <span className="text-gray-300">{availableDateFormatted}</span>
+      <div
+        className={classNames(
+          "rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500",
+          { "bg-green-500": isActiveLesson }
+        )}
+      >
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
-            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <span
+              className={classNames(
+                "text-smfont-medium flex items-center gap-2",
+                {
+                  "text-white": isActiveLesson,
+                  "text-blue-500": !isActiveLesson,
+                }
+              )}
+            >
               <CheckCircle size={20} />
               Conteúdo liberado
             </span>
@@ -36,11 +50,26 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
               Em Breve
             </span>
           )}
-          <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
+          <span
+            className={classNames(
+              "text-xs rounded px-2 py-[0.125rem] text-white border  font-bold",
+              {
+                "border-white": isActiveLesson,
+                "border-green-300": !isActiveLesson,
+              }
+            )}
+          >
             {type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
           </span>
         </header>
-        <strong className="text-gray-200 mt-5 block">{title}</strong>
+        <strong
+          className={classNames(" mt-5 block", {
+            "text-white": isActiveLesson,
+            "text-gray-200": !isActiveLesson,
+          })}
+        >
+          {title}
+        </strong>
       </div>
     </Link>
   );
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -7,55 +7,25 @@ import {
 } from "phosphor-react";
 
 import "@vime/core/themes/default.css";
-import { gql, useQuery } from '@apollo/client';
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String){
-    lesson(where: {slug: $slug}){
-      title
-      videoId
-      description
-      teacher{
-        bio
-        avatarURL
-        name
-      }
-
-    }
-  }
-`
-
-interface GetLessonBySlugResponse{
-  lessson:{
-    title: string,
-    videoId: string,
-    description: string,
-    teacher:{
-    bio: string,
-    avatarURL: string,
-    name: string,
-    }
-  }
-}
-
-interface VideoProps{
+interface VideoProps {
   lessonSlug: string;
 }
 
-export function Video({lessonSlug}:VideoProps) {
-
-  const {data} = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+export function Video({ lessonSlug }: VideoProps) {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
-      slug: lessonSlug
-    }
-  })
+      slug: lessonSlug,
+    },
+  });
 
-  if(!data){
-    return(
+  if (!data || !data.lesson) {
+    return (
       <div className="flex-1">
         <p>Carregando...</p>
       </div>
-    )
+    );
   }
 
   return (
@@ -63,7 +33,7 @@ export function Video({lessonSlug}:VideoProps) {
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId={data.lessson.videoId} />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -71,25 +41,27 @@ export function Video({lessonSlug}:VideoProps) {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">
-            {data.lessson.title}
-            </h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-            {data.lessson.description}
+              {data.lesson.description}
             </p>
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data.lessson.teacher.avatarURL}
-                alt=""
-              />
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">{data.lessson.teacher.name}</strong>
-                <span className="text-gray-200 text-sm block">
-                {data.lessson.teacher.bio}
-                </span>
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src={data.lesson.teacher.avatarURL}
+                  alt=""
+                />
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">
+                    {data.lesson.teacher.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block">
+                    {data.lesson.teacher.bio}
+                  </span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
           <div className="flex flex-col gap-4">
             <a
