0e608b107467ddb0f3000542b4a7ac25f8240d9c
Darlei Sidegum <darleidasilvasidegum@hotmail.com>
2022-06-28 14:47:36 -0300

    ajustes

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -8,6 +8,7 @@ import {
 
 import "@vime/core/themes/default.css";
 import { useGetLessonBySlugQuery } from "../graphql/generated";
+import { useEffect, useState } from "react";
 
 interface VideoProps {
   lessonSlug: string;
@@ -32,7 +33,7 @@ export function Video({ lessonSlug }: VideoProps) {
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
-          <Player>
+          <Player key={lessonSlug}>
             <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
