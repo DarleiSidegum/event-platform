aee433e6499aff566fe0e3894039fd91e7708e37
Darlei Sidegum <darleidasilvasidegum@hotmail.com>
2022-06-30 18:03:37 -0300

    ajustes

---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export function Login() {
+  return (
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto"></div>
+    </div>
+  );
+}
